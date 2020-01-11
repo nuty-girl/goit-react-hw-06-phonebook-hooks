bb35819b4526465e71b0a7e20ec20e3d04a8e150
unknown <nuty-girl@mail.ru>
2020-01-11 20:50:19 +0200

    Updates

---
+++ precache-manifest.735f2f0ebfda7e2dfc6b82f525cb78a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c746410167f71e53d05de17fa40b51f",
+    "url": "/goit-react-hw-06-phonebook-hooks/index.html"
+  },
+  {
+    "revision": "4883304da95868afe1e8",
+    "url": "/goit-react-hw-06-phonebook-hooks/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d2cd3548d9288af22a0",
+    "url": "/goit-react-hw-06-phonebook-hooks/static/js/2.b2107e57.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook-hooks/static/js/2.b2107e57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4883304da95868afe1e8",
+    "url": "/goit-react-hw-06-phonebook-hooks/static/js/main.a5d508da.chunk.js"
+  },
+  {
+    "revision": "ffa78ea8cdb0f5346bca",
+    "url": "/goit-react-hw-06-phonebook-hooks/static/js/runtime-main.45d2df27.js"
+  }
+]);
